4c4976d521f71778f864a97415589167429220a1
Steven Nagie <sanagie@yahoo.com>
2017-01-25 18:02:04 -0700

    Selenium actually working (opening page, checking info.

--- compareTime2.js
+++ compareTime2.js
@@ -24,25 +24,43 @@ const webdriver = require('selenium-webdriver'),
 
 chai.use(chaiAsPromised)
 
+// Creates driver as globally accessible variable
 var driver;
 
+// Assigns driver var to a new instance of webdriver, including Capabilities specific to chrome.
 before(function() {
   driver = new webdriver.Builder()
       .withCapabilities(webdriver.Capabilities.chrome())
       .build();
-      driver.getWindowHandle();
+  // Selenium automatically assigns an alphanumeric id to each window as soon as the object exists. It uses this to switch control among several windows.
+  // This command gets the window handle of the current window.
+  driver.getWindowHandle();
 });
 
-after(function() {
-  driver.quit();
+// Quits the driver after everything is done. Closes all browser windows and safely ends the session.
+after(function(done) {
+  driver.quit().then(done);
 });
 
 describe('Webdriver tutorial', function() {
-  beforeEach(function() {
-    driver.get("https://team.goodeggs.com/getting-started-with-selenium-webdriver-for-node-js-f262a00c52e1#.wxv6tqxi6");
+  this.timeout(25000);
+  // Grabs the webpage we're interested in. Have to include .then(done) to get it working properly.
+  beforeEach(function(done) {
+    driver.get("https://team.goodeggs.com/getting-started-with-selenium-webdriver-for-node-js-f262a00c52e1#.wxv6tqxi6").then(done);
   });
 
+
+  // Gets the title of the webpage we've grabbed in/with the driver, and compares it to what we want it to be. Note that it delivers it in the form of a promise, hence the need for "eventually" from chai-as-promised.
   it("has the title of the post in the window's title", function() {
     expect(driver.getTitle()).to.eventually.contain('Getting started with Selenium Webdriver for node.js');
+  });
+
+  it("has publication date", function() {
+    // Returns a promise, which is why we're using chai-as-promised "eventually"
+    // Doesn't handle errors properly
+    var date = driver.findElement(By.name('.post')).getText();
+    expect(date).to.eventually.equal('December 30th, 2014');
   })
+
+
 })
